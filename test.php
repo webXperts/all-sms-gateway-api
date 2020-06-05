184b68345f775024bb2050d235b95aef82b8b22c
angryboy <webxpert001@gmail.com>
2020-06-05 17:11:19 +0600

    modify

--- test.php
+++ test.php
@@ -2,24 +2,24 @@
 
 // Send Singe SMS Via HTTP / SMPP Gateway
 
-// require_once('autoload.php');
+require_once('autoload.php');
 
-// try {
+try {
 
-// 	$mobile_no = '01737346122';
-// 	$message = 'do you like sport?';
-// 	$sender_id = 'dfewrty56yu';
-// 	$country_id = 14;
-// 	$gateway = 'mimsms';
-// 	$data_type = 'Plain'; // Plain/Unicode
-// 	$response = $apiClient->sendSMSviaHttp($mobile_no, $message, $sender_id, $country_id, $gateway, $data_type);
+	$mobile_no = '01737346122';
+	$message = 'do you like sport?';
+	$sender_id = 'dfewrty56yu';
+	$country_id = 14;
+	$gateway = 'mimsms';
+	$data_type = 'Plain'; // Plain/Unicode
+	$response = $apiClient->sendSMSviaHttp($mobile_no, $message, $sender_id, $country_id, $gateway, $data_type);
 
-// 	dd($response);
+	dd($response);
 
-// } catch (Exception $e) {
+} catch (Exception $e) {
     
-//     echo $e->getMessage();
-// }
+    echo $e->getMessage();
+}
 
 
 /*
@@ -200,28 +200,28 @@ Array
 
 // Send Multiple SMS Via Android Gateway
 
-require_once('autoload.php');
+// require_once('autoload.php');
 
-try {
+// try {
 
-    $mobile_numbers = array(
-        '01737346122',
-        '01303595747',
-    );
+//     $mobile_numbers = array(
+//         '01737346122',
+//         '01303595747',
+//     );
 
-    $message = 'do you like sport?';
-	$device_id = 1;
-	$sim_id = 99;
-	$data_type = 'Plain';
-	$send_at = 'now';
-    $response = $apiClient->sendMultipleSMS($mobile_numbers, $message, $device_id, $sim_id, $data_type, $send_at);
+//     $message = 'do you like sport?';
+// 	$device_id = 1;
+// 	$sim_id = 99;
+// 	$data_type = 'Plain';
+// 	$send_at = 'now';
+//     $response = $apiClient->sendMultipleSMS($mobile_numbers, $message, $device_id, $sim_id, $data_type, $send_at);
 
-    dd($response);
+//     dd($response);
 
-} catch (Exception $e) {
+// } catch (Exception $e) {
     
-    echo $e->getMessage();
-}
+//     echo $e->getMessage();
+// }
 
 /*
 
